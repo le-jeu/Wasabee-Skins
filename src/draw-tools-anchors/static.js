41d7b81ae34faf4e9d49e8153231e4e792b1bfb8
LeJeu <64744459+le-jeu@users.noreply.github.com>
2022-07-04 07:41:46 +0200

    fix: CSS is required atm

--- src/draw-tools-anchors/static.js
+++ src/draw-tools-anchors/static.js
@@ -3,7 +3,8 @@ import anchor from "./images/marker_layer_custom.svg";
 const skin = {};
 
 skin.static = {
-  anchorTemplate: anchor
+  anchorTemplate: anchor,
+  CSS: {},
 };
 
 export default skin;
--- src/thegame-svg/static.js
+++ src/thegame-svg/static.js
@@ -3,6 +3,7 @@ import anchor from "./images/marker_layer_custom.svg";
 const skin = {};
 skin.static = {
   anchorTemplate: anchor,
+  CSS: {},
 };
 
 export default skin;
