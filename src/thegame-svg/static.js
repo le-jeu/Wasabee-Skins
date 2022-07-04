2e6facd4d04e4265e6771c0cdeb346a9e6c0f73a
LeJeu <64744459+le-jeu@users.noreply.github.com>
2022-07-04 07:33:05 +0200

    fix: old way in thegame

--- src/thegame-svg/static.js
+++ src/thegame-svg/static.js
@@ -1,10 +1,6 @@
 import anchor from "./images/marker_layer_custom.svg";
 
 const skin = {};
-
-// each skin needs its own namespace, change that here
-window.plugin.wasabeeSkins.thegame_svg = skin;
-
 skin.static = {
   anchorTemplate: anchor,
 };
